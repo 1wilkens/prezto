f75cbaff6978b1bb7ae5146b33898b5f879765b1
Florian Wilkens <wilkens@informatik.uni-hamburg.de>
2021-04-14 12:46:20 +0200

    utility: use exa if present

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -113,16 +113,25 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
+# support exa
+if is-callable 'exa'; then
+    alias ls="exa"
+    alias l='ls -1a'         # Lists in one column, hidden files.
+    alias ll='ls -l'         # Lists human readable sizes.
+    alias la='ll -a'         # Lists human readable sizes, hidden files.
+else
+    alias l='ls -1A'         # Lists in one column, hidden files.
+    alias ll='ls -lh'        # Lists human readable sizes.
+    alias la='ll -A'         # Lists human readable sizes, hidden files.
+    alias lx='ll -XB'        # Lists sorted by extension (GNU only).
+    alias lk='ll -Sr'        # Lists sorted by size, largest last.
+    alias lt='ll -tr'        # Lists sorted by date, most recent last.
+    alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+    alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+fi
+
 alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 alias sl='ls'            # I often screw this up.
 
 # Grep
