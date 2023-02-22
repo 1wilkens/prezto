b619c350ee98d319ecbd68c294aad9ee0efb7dc4
Florian Wilkens <gh@1wilkens.org>
2023-02-22 16:29:39 +0100

    customizations

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -113,15 +113,24 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
+# support exa
+if is-callable 'exa'; then
+    alias ls="exa --group-directories-first"
+    alias l='ls -1a'         # Lists in one column, hidden files.
+    alias ll='ls -l'         # Lists human readable sizes.
+    alias la='ll -a'         # Lists human readable sizes, hidden files.
+else
+    alias l='ls -1A'         # Lists in one column, hidden files.
+    alias ll='ls -lh'        # Lists human readable sizes.
+    alias la='ll -A'         # Lists human readable sizes, hidden files.
+    alias lk='ll -Sr'        # Lists sorted by size, largest last.
+    alias lt='ll -tr'        # Lists sorted by date, most recent last.
+    alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+    alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+fi
+
 alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 alias sl='ls'            # Correction for common spelling error.
 
 if [[ ${(@M)${(f)"$(ls --version 2>&1)"}:#*GNU *} ]]; then
